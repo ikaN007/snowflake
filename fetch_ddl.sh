5557df6260e1649ac2393559e244e58aa580d09b
yatin-sawant-od <55287197+yatin-sawant-od@users.noreply.github.com>
2019-12-17 16:40:39 +0530

    Update fetch_ddl.sh

--- fetch_ddl.sh
+++ fetch_ddl.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-count=`cat Objectlist.txt | wc -l`
-for rotate in `seq 1 "$count"`;
+count=`cat /home/jenkins/agent/workspace/Snowflake_test_test_dev/Objectlist.txt | wc -l`
+for rotate in `seq 1 "$count"`
 do
-awk 'NR == n' n="${rotate}" Objectlist.txt > oneline.txt
-fqdn=`awk '{ print $1 }' oneline.txt`
-objtype=`awk '{ print $2 }' oneline.txt`
-snowsql -q "SELECT GET_DDL('$objtype','$fqdn')" | grep -v GET_DDL > BIDW.${fqdn}.${objtype}.sql; chmod a+x BIDW.${fqdn}.${objtype}.sql
+awk 'NR == n' n="${rotate}" /home/jenkins/agent/workspace/Snowflake_test_test_dev/Objectlist.txt > /home/jenkins/agent/workspace/Snowflake_test_test_dev/oneline.txt
+fqdn=`awk '{ print $1 }' /home/jenkins/agent/workspace/Snowflake_test_test_dev/oneline.txt`
+objtype=`awk '{ print $2 }' /home/jenkins/agent/workspace/Snowflake_test_test_dev/oneline.txt`
+snowsql -q "SELECT GET_DDL('$objtype','$fqdn')" | grep -v GET_DDL > /home/jenkins/agent/workspace/Snowflake_test_test_dev/BIDW.${fqdn}.${objtype}.sql; chmod a+x /home/jenkins/agent/workspace/Snowflake_test_test_dev/BIDW.${fqdn}.${objtype}.sql
 done
