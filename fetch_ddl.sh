c3932181780e9f5af4f17dbc658ee269ccf145f9
yatin-sawant-od <55287197+yatin-sawant-od@users.noreply.github.com>
2019-12-17 16:20:52 +0530

    Add files via upload

---
+++ fetch_ddl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+count=`cat Objectlist.txt | wc -l`
+for rotate in `seq 1 "$count"`
+do
+awk 'NR == n' n="${rotate}" Objectlist.txt > oneline.txt
+fqdn=`awk '{ print $1 }' oneline.txt`
+objtype=`awk '{ print $2 }' oneline.txt`
+snowsql -q "SELECT GET_DDL('$objtype','$fqdn')" | grep -v GET_DDL > BIDW.${fqdn}.${objtype}; chmod a+x BIDW.${fqdn}.${objtype}
+done
