c2e05cce4943ad9ccd7d8f801600946b4185e4f5
devopsyatin <sawant.yatin@yahoo.in>
2019-12-18 14:54:12 +0000

    adding the reviewed file

---
+++ BIDW.JENKINSTEST.TEST_VIEW.VIEW.sql
@@ -0,0 +1,3 @@
+                                                                     
+CREATE VIEW TEST_VIEW COMMENT='Test view' AS SELECT NAME FROM TEST01;
+                                                                     
