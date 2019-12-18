72b29f632d7924f531e4d46fdd052fda903addf2
devopsyatin <sawant.yatin@yahoo.in>
2019-12-18 16:53:47 +0000

    adding the reviewed file

---
+++ BIDW.JENKINSTEST.TEST01.TABLE.sql
@@ -0,0 +1,5 @@
+create or replace TABLE TEST01 (     
+	"S. No" NUMBER(38,0),                                                                            
+	NAME VARCHAR(16777216)                                                                            
+)COMMENT='tables'                    
+;                                    
