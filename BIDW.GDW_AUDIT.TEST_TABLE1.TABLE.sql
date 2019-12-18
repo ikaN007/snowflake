e09a67ba312999dd46cb248479e788842bdb7325
yatin-sawant-od <55287197+yatin-sawant-od@users.noreply.github.com>
2019-12-18 15:27:24 +0530

    Delete BIDW.GDW_AUDIT.TEST_TABLE1.TABLE.sql

--- BIDW.GDW_AUDIT.TEST_TABLE1.TABLE.sql
+++
@@ -1,4 +0,0 @@
-create or replace TABLE TEST_TABLE1 (   
-	EMP_ID NUMBER(18,2),                                                                                  
-	EMP_NAME VARCHAR(10)                                                                                  
-);                                      
