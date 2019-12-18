0083ff2eedf0fe75c3fa180a64b3173df573cdf8
yatin-sawant-od <55287197+yatin-sawant-od@users.noreply.github.com>
2019-12-18 19:55:25 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,12 @@
 # Creation Date: 18 Dec 2019      #
 ###################################
 
-ls -larth Objectlist_qa.txt | awk '{ print $NF }' > full_bidw_file_listing.txt
-ls -larth Objectlist_qa.txt | awk '{ print $NF }' > bidw_file_listing.txt
+ls -larth /home/jenkins/agent/workspace/Snowflake_test_qa/Objectlist_qa.txt | awk '{ print $NF }' > /home/jenkins/agent/workspace/Snowflake_test_qa/full_bidw_file_listing.txt
+ls -larth /home/jenkins/agent/workspace/Snowflake_test_qa/Objectlist_qa.txt | awk '{ print $NF }' > /home/jenkins/agent/workspace/Snowflake_test_qa/bidw_file_listing.txt
 
 ######################## Variable Declaration #################
-actual_file_count=`ls -1 Objectlist_qa.txt | wc -l`
-file_files_count=`cat bidw_file_listing.txt | grep "BIDW" | wc -l`
+actual_file_count=`ls -1 /home/jenkins/agent/workspace/Snowflake_test_qa/Objectlist_qa.txt | wc -l`
+file_files_count=`cat /home/jenkins/agent/workspace/Snowflake_test_qa/bidw_file_listing.txt | grep "BIDW" | wc -l`
 DB_NAME=TEST_QA
 
 echo "The number of actual files to process are $actual_file_count"
@@ -19,15 +19,15 @@ echo "The number of file files to process are $file_files_count"
 
 for search in `seq 1 "$file_files_count"`
 do
-awk 'NR == n' n="${search}" bidw_file_listing.txt > bidw_file_one_per_line.txt
-awk 'NR == n' n="${search}" full_bidw_file_listing.txt > bidw_full_file_one_per_line.txt
+awk 'NR == n' n="${search}" /home/jenkins/agent/workspace/Snowflake_test_qa/bidw_file_listing.txt > /home/jenkins/agent/workspace/Snowflake_test_qa/bidw_file_one_per_line.txt
+awk 'NR == n' n="${search}" /home/jenkins/agent/workspace/Snowflake_test_qa/full_bidw_file_listing.txt > /home/jenkins/agent/workspace/Snowflake_test_qa/bidw_full_file_one_per_line.txt
 
 ###################### Accept each parameter as variable ###################
 
-fqdn=`cat bidw_file_one_per_line.txt | awk -F "." '{ print $2"."$3 }'`
-object=`cat bidw_file_one_per_line.txt | awk -F "." '{ print $(NF-1) }'`
-schema=`cat bidw_file_one_per_line.txt | awk -F "." '{ print $2 }'`
-filename=`cat bidw_full_file_one_per_line.txt`
+fqdn=`cat /home/jenkins/agent/workspace/Snowflake_test_qa/bidw_file_one_per_line.txt | awk -F "." '{ print $2"."$3 }'`
+object=`cat /home/jenkins/agent/workspace/Snowflake_test_qa/bidw_file_one_per_line.txt | awk -F "." '{ print $(NF-1) }'`
+schema=`cat /home/jenkins/agent/workspace/Snowflake_test_qa/bidw_file_one_per_line.txt | awk -F "." '{ print $2 }'`
+filename=`cat /home/jenkins/agent/workspace/Snowflake_test_qa/bidw_full_file_one_per_line.txt`
 
 
 #### Format for exection of command: echo "snowsql -f filename.sql -q USE DATABASE variable USE SCHEMA variable"
