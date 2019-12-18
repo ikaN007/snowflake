986ef03b5ae7ebe896da9bc685370f2ebaffc655
devopsyatin <49586839+devopsyatin@users.noreply.github.com>
2019-12-18 19:03:59 +0530

    Update fetch_ddl.sh

--- fetch_ddl.sh
+++ fetch_ddl.sh
@@ -7,3 +7,5 @@ fqdn=`awk '{ print $1 }' oneline.txt`
 objtype=`awk '{ print $2 }' oneline.txt`
 snowsql -q "SELECT GET_DDL('$objtype','$fqdn')" | grep -v GET_DDL > BIDW.${fqdn}.${objtype}.sql; chmod a+x BIDW.${fqdn}.${objtype}.sql
 done
+
+ls -larth BIDW.*.sql | awk '{ print $NF }' > Objectlist_qa.txt
