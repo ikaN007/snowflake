b855614c4107d2cf19b80da321bef4f9abbaf8ff
yatin-sawant-od <yatin.sawant@officedepot.com>
2019-12-18 10:30:21 +0000

    adding the reviewed file

---
+++ BIDW.GDW_AUDIT.TEST_TABLE1.TABLE.sql
@@ -0,0 +1,4 @@
+create or replace TABLE TEST_TABLE1 (   
+	EMP_ID NUMBER(18,2),                                                                                  
+	EMP_NAME VARCHAR(10)                                                                                  
+);                                      
