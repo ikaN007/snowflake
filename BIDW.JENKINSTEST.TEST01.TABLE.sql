e63b1eeb8280d1be073eadaa08b49e0d91c82680
yatin-sawant-od <55287197+yatin-sawant-od@users.noreply.github.com>
2019-12-18 22:19:12 +0530

    Delete BIDW.JENKINSTEST.TEST01.TABLE.sql

--- BIDW.JENKINSTEST.TEST01.TABLE.sql
+++
@@ -1,5 +0,0 @@
-create or replace TABLE TEST01 (     
-	"S. No" NUMBER(38,0),                                                                            
-	NAME VARCHAR(16777216)                                                                            
-)COMMENT='tables'                    
-;                                    
