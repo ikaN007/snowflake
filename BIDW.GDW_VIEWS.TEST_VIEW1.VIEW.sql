c1234e6fbc48f4c2dbbe04bdae4f641c7d0b1675
devopsyatin <49586839+devopsyatin@users.noreply.github.com>
2019-12-18 19:12:26 +0530

    Delete BIDW.GDW_VIEWS.TEST_VIEW1.VIEW.sql

--- BIDW.GDW_VIEWS.TEST_VIEW1.VIEW.sql
+++
@@ -1,3 +0,0 @@
-                                                                   
-CREATE VIEW GDW_VIEWS.TEST_VIEW1 AS SELECT * FROM DBA_DB.TEST_TBL1;
-                                                                   
