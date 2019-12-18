6220551d09d58f0f3bc81d91ea8fe38b0828402c
yatin-sawant-od <55287197+yatin-sawant-od@users.noreply.github.com>
2019-12-18 19:47:08 +0530

    Delete BIDW.JENKINSTEST.TEST_VIEW.VIEW.sql

--- BIDW.JENKINSTEST.TEST_VIEW.VIEW.sql
+++
@@ -1,3 +0,0 @@
-                                                                     
-CREATE VIEW TEST_VIEW COMMENT='Test view' AS SELECT NAME FROM TEST01;
-                                                                     
